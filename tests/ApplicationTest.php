add8803f1b87ed634d4074bbf53c24a11a311483
Tony NGUEREZA <nguerezatony@gmail.com>
2021-05-25 11:11:48 +0100

    Initial commit

---
+++ tests/ApplicationTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Platine\Test\Console;
+
+use Platine\Console\Application;
+use Platine\PlatineTestCase;
+
+/**
+ * Console class tests
+ *
+ * @group core
+ * @group console
+ */
+class ApplicationTest extends PlatineTestCase
+{
+
+    public function testConstructor(): void
+    {
+        $s = new Application();
+
+        $this->assertInstanceOf(Application::class, $s);
+    }
+}
